938f00030c02807e92e76adba8c58a357289d50d
sun-xue-shi <2595531421@qq.com>
2024-09-17 20:02:43 +0800

    feat:添加finalComp组件

---
+++ src/components/FinalComp/FinalComp.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="final-page" :style="page && page.props">
+    <div v-for="item in components" :key="item.id" :id="`component-${item.id}`">
+      <component :is="item.name" v-bind="item.props" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import type { ComponentData, PageData } from '@/common/defaultProps'
+
+defineProps<{
+  page: PageData
+  components: ComponentData[]
+}>()
+</script>
